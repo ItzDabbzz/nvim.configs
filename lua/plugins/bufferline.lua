8a02f6d65d4cb6f05b48019ea3e168b803d4de04
Dabz <itzdabbzz@gmail.com>
2024-03-24 16:20:58 -0500

    Adjust bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,45 +4,31 @@ return {
 		"akinsho/bufferline.nvim",
 		version = "*",
 		dependencies = "nvim-tree/nvim-web-devicons",
-		opts = {},
-		config = function()
-			local bufferline = require("bufferline")
-
-			bufferline.setup({
-				options = {
-					mode = "buffers",
-					themable = true,
-					indicator = {
-						icon = '▎', -- this should be omitted if indicator style is not 'icon'
-						style = 'icon',
-					},
-					buffer_close_icon = '󰅖',
-					modified_icon = '●',
-					close_icon = '',
-					left_trunc_marker = '',
-					right_trunc_marker = '',
-					diagnostics = "nvim_lsp",
-					diagnostics_update_in_insert = false,
-					-- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
-					diagnostics_indicator = function(count, level, diagnostics_dict, context)
-						return "("..count..")"
-					end,
-					offsets = {
-						{
-							filetype = "neo-tree",
-							text = "File Explorer" ,
-							text_align = "center",
-							separator = true
-						}
-					},
-					color_icons = true,
-					show_buffer_icons = true,
-					show_buffer_close_icons = true,
-					show_close_icon = true,
-					show_tab_indicators = true,
-
-				}
-			})
+		opts = function()
+			local Offset = require("bufferline.offset")
+			if not Offset.edgy then
+				local get = Offset.get
+				Offset.get = function()
+					if package.loaded.edgy then
+						local layout = require("edgy.config").layout
+						local ret = { left = "", left_size = 0, right = "", right_size = 0 }
+						for _, pos in ipairs({ "left", "right" }) do
+							local sb = layout[pos]
+							if sb and #sb.wins > 0 then
+								local title = " Sidebar" .. string.rep(" ", sb.bounds.width - 8)
+								ret[pos] = "%#EdgyTitle#" .. title .. "%*" .. "%#WinSeparator#│%*"
+								ret[pos .. "_size"] = sb.bounds.width
+							end
+						end
+						ret.total_size = ret.left_size + ret.right_size
+						if ret.total_size > 0 then
+							return ret
+						end
+					end
+					return get()
+				end
+				Offset.edgy = true
+			end
 		end
 	},
 }
