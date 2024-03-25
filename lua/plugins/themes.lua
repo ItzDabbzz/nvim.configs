3d858729c5e639c55cea73e08cea2b7885b1cb4d
Dabz <itzdabbzz@gmail.com>
2024-03-24 23:34:43 -0500

    Added more themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -13,8 +13,19 @@ return {
 		"rose-pine/nvim",
 		name = "rose-pine",
 	},
-
+ 
 	"sainnhe/everforest",
 
-	"savq/melange-nvim"
+	"savq/melange-nvim",
+
+	-- Kanagawa Theme
+	-- 3 Variants:
+	-- - wave - Heart warming sessions
+	-- - dragon - late night sessions
+	-- - lotus - when you're out in the open
+	-- require("kanagawa").load("wave")
+	-- https://github.com/rebelot/kanagawa.nvim
+	"rebelot/kanagawa.nvim",
+
+	{ "EdenEast/nightfox.nvim" }
 }
